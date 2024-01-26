071d066a860da9a44371ab3bb2f96da7563f4a06
Roope1 <roope70@live.com>
2024-01-26 21:01:09 +0200

    TMP + basic menu

---
+++ FGJ/Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private string _playScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(_playScene);
+
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
