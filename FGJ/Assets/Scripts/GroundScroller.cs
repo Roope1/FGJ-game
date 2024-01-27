3d1aec52bcaffc2522b23364960dc070c808b8ac
Roope1 <roope70@live.com>
2024-01-27 20:49:53 +0200

    ground fix

--- FGJ/Assets/Scripts/GroundScroller.cs
+++ FGJ/Assets/Scripts/GroundScroller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GroundScroller : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
+    [SerializeField] private float _speed = 0.055f;
 
     private float _offset = 0;
 
@@ -17,7 +18,7 @@ public class GroundScroller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        _offset += GameManager.Instance.ObstacleSpeed * 0.12f;
+        _offset += GameManager.Instance.ObstacleSpeed * _speed;
         _renderer.material.SetTextureOffset("_BaseMap", new Vector2(0, _offset));
     }
 }
