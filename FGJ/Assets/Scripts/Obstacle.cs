5b07eb41671cfd3b5c5350c93e7f3c08ac6e768d
Roope1 <roope70@live.com>
2024-01-27 15:53:37 +0200

    obstacle spawning

---
+++ FGJ/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
---
+++ FGJ/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] private float _width = 1f;
+    [SerializeField] private GameObject[] _obstacles;
+    private bool _gameActive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObstacles());
+    }
+
+    private IEnumerator SpawnObstacles()
+    {
+        while (_gameActive)
+        {
+            int obstacleIndex = UnityEngine.Random.Range(0, _obstacles.Count());
+            GameObject newObstacle = Instantiate(
+                _obstacles[obstacleIndex],
+                new Vector3(
+                    UnityEngine.Random.Range(
+                        (float)(transform.position.x - Math.Floor(_width / 2)),
+                        (float)(transform.position.x + Math.Floor(_width / 2))
+                        ),
+                    transform.position.y,
+                    transform.position.z),
+                Quaternion.identity);
+            newObstacle.AddComponent<Obstacle>();
+            yield return new WaitForSeconds(4f);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, new Vector3(_width, 1, 1));
+    }
+}
