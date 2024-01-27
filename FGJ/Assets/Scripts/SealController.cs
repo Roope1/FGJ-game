4a414323fe6c89947fc087083ffb3e545d6ab59a
Roope1 <roope70@live.com>
2024-01-27 14:50:29 +0200

    seal stuff started

---
+++ FGJ/Assets/Scripts/SealController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealController : MonoBehaviour
+{
+    [SerializeField] private float _moveForce = 2f;
+    private Rigidbody _sealRig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sealRig = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Move(Input.GetAxis("Horizontal"));
+        }
+    }
+
+    private void Move(float input)
+    {
+        _sealRig.AddForce(Vector3.left * input * _moveForce, ForceMode.Force);
+    }
+
+}
