97740fa3fb309e6cefc1b63ca58f13f410bca1d8
Roope1 <roope70@live.com>
2024-01-28 02:21:56 +0200

    aspect ratio forcing

--- FGJ/Assets/Scripts/Menu.cs
+++ FGJ/Assets/Scripts/Menu.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField] private string _playScene;
+    [SerializeField] private TMP_Text _highScore;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        _highScore.SetText($"High Score: {highScore}");
     }
 
     // Update is called once per frame
